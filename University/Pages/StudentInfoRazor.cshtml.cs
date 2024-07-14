20fca4b65de207858fe5c573cf3c785a198f34eb
Abdullah-Haytham <hedeya@gmail.com>
2024-07-14 12:31:47 +0300

    Implemented Student Info using razor only

---
+++ University/Pages/StudentInfoRazor.cshtml.cs
@@ -0,0 +1,64 @@
+using EdgeDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using University.Models;
+
+namespace University.Pages
+{
+    public class StudentInfoRazorModel : PageModel
+    {
+        private readonly EdgeDBClient _client;
+        [BindProperty]
+        public string StudentId { get; set; } = string.Empty;
+
+        [BindProperty]
+        public StudentModel Student { get; set; } = new();
+
+        public StudentInfoRazorModel(EdgeDBClient client)
+        {
+            _client = client;
+        }
+        public void OnGet()
+        {
+            
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (string.IsNullOrWhiteSpace(StudentId))
+            {
+                return Page();
+            }
+            else
+            {
+                string query = @"
+            SELECT Student {
+                student_id,
+                name,
+                age,
+                courses: {
+                    name,
+                    code,
+                    credit,
+                    department: {
+                        name,
+                        code
+                    },
+                    students
+                },
+                department: {
+                    name,
+                    code,
+                    students,
+                }
+            }
+            FILTER .student_id = <str>$studentId";
+                Student = await _client.QuerySingleAsync<StudentModel>(query, new Dictionary<string, Object?>
+        {
+            {"studentId", StudentId},
+        });
+                return Page();
+            }  
+        }
+    }
+}
