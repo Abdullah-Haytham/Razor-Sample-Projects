080a273f158f0083b01c025eda505c3d057f615b
Abdullah-Haytham <hedeya@gmail.com>
2024-07-14 09:27:11 +0300

    Added student model

---
+++ University/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? StudentId { get; set; }
+        [Required]
+        public int? Age { get; set; }
+        public Course[] Courses { get; set; } = [];
+        public Department? Department { get; set; }
+    }
+}
