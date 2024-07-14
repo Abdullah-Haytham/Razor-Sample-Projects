ca6a7ef0db51106d0cccc2bb969cbb6ad43ac0f5
Abdullah-Haytham <hedeya@gmail.com>
2024-07-14 16:26:41 +0300

    Added Account Model

---
+++ University/Models/AccountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace University.Models
+{
+    public class AccountModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
